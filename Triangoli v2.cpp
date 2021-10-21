0c07a98dc5c0b9a74b704999b7b412df6baa674d
MicheleMeazzini <92431763+MicheleMeazzini@users.noreply.github.com>
2021-10-21 16:25:58 +0200

    Update Triangoli v2.cpp

--- Triangoli v2.cpp
+++ Triangoli v2.cpp
@@ -2,36 +2,39 @@
 using namespace std;
 
 int main (){
-    int n, g, f, c;
+    int n, g, i;
     cout<< "inserire numero naturale dispari:";
     cin>>n;
     g=n;
 
     cout << "Triangolo isoscele" << endl;
 
-    for (int i = 0; i < n; ++i) {
-        for (int p = 0; p < i; ++p) {
+    for (int l = 0; l < (n / 2) + 1; ++l) {
+        for (int p = 0; p < l; ++p) {
             cout << "   ";
         }
         for (int j = 0; j < g; ++j) {
-            cout<<"*  ";
+            cout << "*  ";
         }
         g = g - 2;
         cout << endl;
     }
 
     cout <<"Triangolo isoscele di lato" << endl;
-    /*for (int j = 0; j < n; j++){
-        for (int i = 0; i < c; i++)
+    for (i = 1; i < (n / 2) + 1; ++i) {
+        for (int j = 0; j < i; ++j) {
             cout << "*  ";
-        if (c < (n / 2) + 1)
-            c++;
-        for (int i = 0 ; i < c; i++)
-            cout << "*  ";
-
-
-
+        }
         cout << endl;
-    }*/
+    }
+    if (i == (n / 2) + 1){
+        for (int j = (n / 2) + 1; j > 0; --j) {
+            for (int k = 0; k < j; ++k) {
+                cout << "*  ";
+            }
+            cout << endl;
+        }
+    }
     return 0;
 }
+
